ca0a31a60df62150e7e48422836fb456c31235b6
Scott Weinman <itsmymacguy@gmail.com>
2021-03-17 13:39:23 -0400

    got Goal status_order to auto increment so that it sorts properly

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,5 +2,15 @@ class Status < ApplicationRecord
   has_many :tokis, dependent: :destroy
 
   validates_numericality_of :status_order,  greater_than_or_equal_to: 1
-  validates_uniqueness_of :status, :status_order
+  validates_uniqueness_of :status
+
+  validate :proper_status_order
+
+  def proper_status_order
+    while Status.find_by_status_order(status_order).present?
+      new_status_order = Status.find_by_status_order(status_order).status_order + 1
+      Status.find_by_status_order(status_order).update(status_order: new_status_order)
+    end
+    return true
+  end
 end
